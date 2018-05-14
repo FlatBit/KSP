ab737d9b302fd21e9c7cdaf6b1205e9a77aeeca3
epln60 <eric.peulings@mni.thm.de>
2018-05-14 10:56:43 +0200

    Aufgabe 2

---
+++ asmdef.h
@@ -0,0 +1,23 @@
+
+//Makros
+#define IMMEDIATE(x) ((x) & 0x00FFFFFF)
+#define SIGN_EXTEND(i) ((i) & 0x00800000 ? (i) | 0xFF000000 : (i)) 
+
+//Assembler Befehle
+#define HALT 0
+#define PUSHC 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define MOD 6
+#define RDINT 7
+#define WRINT 8
+#define RDCHR 9
+#define WRCHR 10
+#define PUSHG 11
+#define POPG 12
+#define ASF 13
+#define RSF 14
+#define PUSHL 15
+#define POPL 16
